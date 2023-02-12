5950ab5fd97bcdae979a6fdefc68f59a6d9159b1
Khoceng <90541574+putuuu@users.noreply.github.com>
2023-02-12 14:29:55 +0700

    update profile

--- app/src/main/java/com/example/tugasbnv/model/Akun.kt
+++ app/src/main/java/com/example/tugasbnv/model/Akun.kt
@@ -3,5 +3,4 @@ package com.example.tugasbnv.model
 data class Akun(
     var username :String,
     var email : String,
-    var urlpicture : String
 )
--- app/src/main/java/com/example/tugasbnv/ui/account.kt
+++ app/src/main/java/com/example/tugasbnv/ui/account.kt
@@ -1,9 +1,14 @@
 package com.example.tugasbnv.ui
 
+import android.app.Activity.RESULT_OK
+import android.app.DownloadManager.Request
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.Gravity
 import androidx.fragment.app.Fragment
@@ -17,19 +22,29 @@ import com.example.tugasbnv.R
 import com.example.tugasbnv.model.Akun
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
+import com.squareup.picasso.Picasso
+import java.io.ByteArrayOutputStream
+import java.net.URL
 
 class account : Fragment() {
+
+    companion object {
+        const val REQUEST_CAMERA = 100
+    }
 
     private lateinit var profilpicture: ImageView
     val db = FirebaseFirestore.getInstance()
     val firebaseAuth = FirebaseAuth.getInstance()
     val firebaseUser = firebaseAuth.currentUser
 
-    var akun = Akun("", "", "")
+
+    var akun = Akun("", "")
+    private lateinit var imageurl : Uri
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,13 +60,18 @@ class account : Fragment() {
         val logOut = view.findViewById<Button>(R.id.button3)
 
         if (firebaseUser != null) {
+//            if (firebaseUser.photoUrl != null){
+//                Picasso.get().load(firebaseUser.photoUrl).into(profilpicture)
+//            }else{
+//                Picasso.get().load("https://picsum.photos/seed/picsum/200/300").into(profilpicture)
+//            }
+
             db.collection("users").get()
                 .addOnSuccessListener { result ->
                     for (document in result) {
                         if (document.id == firebaseUser.uid) {
                             val username = document.get("username").toString()
                             val email = document.get("email").toString()
-                            val url = document.get("url").toString()
 
                             val ref = db.collection("users").document(firebaseUser.uid)
                                 .collection("books")
@@ -60,7 +80,7 @@ class account : Fragment() {
                                 tvBuku.text = count.toString()
                             }
 
-                            akun = Akun(username, email, url)
+                            akun = Akun(username, email)
                             tvemail.text = akun.email
                             tvusername.text = akun.username
                         }
@@ -111,13 +131,59 @@ class account : Fragment() {
 
         }
 
-        profilpicture.setOnClickListener {
-            Toast.makeText(profilpicture.context, akun.username, Toast.LENGTH_SHORT).show()
-        }
+//        profilpicture.setOnClickListener {
+//            intentCamera()
+//        }
 
         return view
 
     }
+
+//    private fun intentCamera() {
+//        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {intent ->
+//            activity?.packageManager?.let {
+//                intent.resolveActivity(it).also {
+//                    startActivityForResult(intent, REQUEST_CAMERA)
+//                }
+//            }
+//        }
+//    }
+//
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK){
+//            val imgBitmap = data?.extras?.get("data") as Bitmap
+//            uploadImage(imgBitmap)
+//        }
+//    }
+//
+//    private fun uploadImage(imgBitmap: Bitmap) {
+//        val byteArrayOutputStream = ByteArrayOutputStream()
+//        val ref = FirebaseStorage.getInstance().reference
+//            .child("img/${FirebaseAuth.getInstance().currentUser?.uid}")
+//
+//        imgBitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+//        val image : ByteArray = byteArrayOutputStream.toByteArray()
+//
+//        ref.putBytes(image)
+//            .addOnCompleteListener{
+//                if(it.isComplete){
+//                    ref.downloadUrl.addOnCompleteListener {
+//                        it.result?.let{
+//                            imageurl = it
+//                            profilpicture.setImageBitmap(imgBitmap)
+//                            val image : Uri = when{
+//                                ::imageurl.isInitialized -> imageurl
+//                                firebaseUser?.photoUrl == null ->
+//                                    Uri.parse("https://picsum.photos/seed/picsum/200/300")
+//                                else -> firebaseUser.photoUrl!!
+//                            }
+//                            UserProfileChangeRequest.Builder().setPhotoUri(image).build()
+//                        }
+//                    }
+//                }
+//            }
+//    }
 
 }
 
